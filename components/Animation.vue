e906647a421b08430b86d1437cbc029e7a92d06b
gabaldon <maria@otherplane.com>
2022-08-31 17:36:36 +0200

    feat(Animation): update animation

--- components/Animation.vue
+++ components/Animation.vue
@@ -1,31 +1,16 @@
 <template>
   <svg
-    viewBox="0 0 720 718"
+    viewBox="0 0 725 718"
     fill="none"
-    class="overflow"
-    @mousemove="(e) => move(e)"
-    @mouseleave="update()"
-    @click="update()"
+    @mouseenter="flip(1)"
+    @mouseleave="flip(-1)"
   >
-    <rect
-      v-for="(rect, index) in newRects"
-      :id="rect.id"
-      :ref="rect.id"
-      :key="index"
-      class="path"
-      :x="rect.x"
-      :y="rect.y"
-      :width="rect.width"
-      :height="rect.height"
-      stroke="#FFCC00"
-      stroke-width="4"
-    />
     <rect
       id="rect-1"
       ref="rect-1"
       class="path"
-      x="2"
-      y="2"
+      :x="p1.x"
+      :y="p1.y"
       width="721"
       height="714"
       stroke="#FFCC00"
@@ -35,8 +20,8 @@
       id="rect-2"
       ref="rect-2"
       class="path"
-      x="185"
-      y="191"
+      :x="p0.x"
+      :y="p0.y"
       width="354"
       height="336"
       stroke="#FFCC00"
@@ -57,69 +42,22 @@
 export default {
   data() {
     return {
-      defaultPaths: [
-        { x: 2, y: 2 },
-        { x: 2, y: 2 },
-        { x: null, y: null },
-      ],
-      overflow: true,
-      newRects: [],
+      p0: { x: 185, y: 191 },
+      p1: { x: 2, y: 2 },
     }
   },
+  mounted() {
+    this.flip(-1)
+  },
   methods: {
-    update() {
-      this.newRects = []
-      this.defaultPaths.forEach((path, i) => {
+    flip(direction) {
+      for (let i = 0; i < 3; i++) {
         const refId = `rect-${i + 1}`
-        const el = this.$refs[refId]
-        this.$gsap.killTweensOf(el)
-        this.$gsap.to(el, 1, {
-          x: path.x,
-          y: path.y,
-          transform: '0, 0',
-        })
-      })
-    },
-    move(e) {
-      let newWidth = 721 + 400
-      let newHeight = 714 + 200
-      let x = this.defaultPaths[0].x - 100
-      let y = this.defaultPaths[0].y - 50
-      let id = 4
-      if (this.newRects.length) {
-        newWidth = this.newRects[this.newRects.length - 1].width * 4
-        newHeight = this.newRects[this.newRects.length - 1].height * 4
-        x = this.newRects[this.newRects.length - 1].x - 200
-        y = this.newRects[this.newRects.length - 1].y - 50
-        id += 1
-      }
-      if (this.newRects.length < 2) {
-        this.newRects.push({
-          x,
-          y,
-          width: newWidth,
-          height: newHeight,
-          id: `rect-${id}`,
-        })
-      }
-      this.defaultPaths.forEach((path, i) => {
-        const refId = `rect-${i + 1}`
-        const el = this.$refs[refId]
-        this.$gsap.killTweensOf(el)
-        this.$gsap.to(el, 0.5 + i, {
-          x: path.x + e.clientX / 3,
-          y: path.y + e.clientY / 3,
-        })
-      })
-      if (this.newRects.length) {
-        this.newRects.forEach((rect, i) => {
-          const refId = `rect-${i + 4}`
-          const el = this.$refs[refId]
-          this.$gsap.killTweensOf(el)
-          this.$gsap.to(el, 0.5 + i, {
-            x: rect.x + e.clientX / 3,
-            y: rect.y + e.clientY / 3,
-          })
+        const path = this.$refs[refId]
+        this.$gsap.killTweensOf(path)
+        this.$gsap.to(path, 1 + i, {
+          scaleX: direction,
+          transformOrigin: '50%, 0%',
         })
       }
     },
@@ -134,11 +72,6 @@ svg {
   top: 0;
   left: 0;
 }
-.overflow {
-  position: absolute;
-  overflow: visible;
-  max-width: 56vw;
-}
 
 #path {
   fill: none;
@@ -150,12 +83,6 @@ svg {
   #rect-1 {
     fill: none;
     stroke-width: 40;
-  }
-
-  .overflow {
-    position: relative;
-    overflow: initial;
-    max-width: max-content;
   }
   #rect-2 {
     fill: #fc0;
